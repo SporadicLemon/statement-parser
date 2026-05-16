079ba6cd695d45f84badaffb1864f005b163f701
Paul Mitchell <paul.mitchell354@gmail.com>
2026-05-16 11:06:29 +0100

    chore: add pdfbox-android dependency for PDF text extraction

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,6 +35,9 @@ kotlin {
             implementation(libs.kotlin.test)
             implementation(libs.kotlinx.coroutines.test)
         }
+        androidMain.dependencies {
+            implementation(libs.pdfbox.android)
+        }
     }
 }
 
